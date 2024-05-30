981028651536d9ea4ee142d769dd27982b51c539
kettleTeacues <teacues@gmail.com>
2024-05-30 15:33:48 +0000

    dashboardを仮実装

--- containers/backend/routers/dashboard.py
+++ containers/backend/routers/dashboard.py
@@ -6,6 +6,18 @@ from models.dashboard import Dashboard
 
 router = APIRouter()
 
-@router.get('/dashboard', tags=['dashboard'])
-def getDashboards() -> List[Dashboard.Get_Response]:
+@router.get('/dashboard', tags=['dashboard']) 
+def get_Dashboards() -> List[Dashboard.Get_Response]:
     return selectDashboards()
+
+@router.post('/dashboard', tags=['dashboard'])
+def post_Dashboard(req: List[Dashboard.Post_Request]) -> List[Dashboard.Get_Response]:
+    return createDashboard(req)
+
+@router.put('/dashboard', tags=['dashboard'])
+def put_Dashboard(req: List[Dashboard.Put_Request]) -> List[Dashboard.Get_Response]:
+    return updateDashboard(req)
+
+@router.post('/dashboard', tags=['dashboard'])
+def delete_Dashboard(req: List[str] = Query(...)):
+    return deleteDashboard(req)
